eca73772b255b350c677d670b6da9d558832719e
Mike Slinn <mslinn@micronauticsresearch.com>
2014-12-13 19:15:13 -0800

    Updated dependencies

--- build.sbt
+++ build.sbt
@@ -1,31 +1,50 @@
-organization  := "cua.li"
+import play.core.PlayVersion.{current => playVersion}
 
-version       := "0.2"
+organization  := "cu.li"
 
-scalaVersion  := "2.10.3"
+version       := "0.2.1"
 
-scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8")
+scalaVersion  := "2.11.4"
 
 resolvers ++= Seq(
   "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
-  "Spray repository" at "http://repo.spray.io/"
+  "Spray repository"    at "http://repo.spray.io/"
 )
 
 libraryDependencies ++= {
-  val akkaV = "2.2.3"
-  val sprayV = "1.2.0"
+  val akkaV  = "2.3.6"
+  val sprayV = "1.3.2"
   Seq(
-//  "org.java-websocket"  %   "Java-WebSocket" % "1.3.1",
-    "io.spray"            %%  "spray-json"     % "1.2.5",
-    "io.spray"            %   "spray-can"      % sprayV,
-    "io.spray"            %   "spray-routing"  % sprayV,
-    "com.typesafe.akka"   %%  "akka-actor"     % akkaV,
-    "com.typesafe.akka"   %%  "akka-testkit"   % akkaV   % "test",
-    "io.spray"            %   "spray-testkit"  % sprayV  % "test",
-    "org.scalatest"       %%  "scalatest"      % "2.0"   % "test",
-    "junit"               %   "junit"          % "4.11"  % "test",
-    "org.specs2"          %%  "specs2"         % "2.2.3" % "test"
+//  "org.java-websocket"  %   "Java-WebSocket" % "1.3.1" withSources(),
+    "io.spray"            %%  "spray-json"     % "1.3.1" withSources(),
+    "io.spray"            %%  "spray-can"      % sprayV withSources(),
+    "io.spray"            %%  "spray-routing"  % sprayV withSources(),
+    "com.typesafe.akka"   %%  "akka-actor"     % akkaV withSources(),
+    //
+    "com.typesafe.akka"   %%  "akka-testkit"   % akkaV    % "test" withSources(),
+    "io.spray"            %%  "spray-testkit"  % sprayV   % "test" withSources(),
+    "org.scalatest"       %%  "scalatest"      % "2.2.2"  % "test" withSources(),
+    "junit"               %   "junit"          % "4.12"   % "test",
+    "org.specs2"          %%  "specs2"         % "2.3.13" % "test"
   )
 }
 
+scalacOptions ++= Seq("-deprecation", "-encoding", "UTF-8", "-feature", "-target:jvm-1.7", "-unchecked",
+  "-Ywarn-adapted-args", "-Ywarn-value-discard", "-Xlint")
+
+javacOptions ++= Seq("-Xlint:deprecation", "-Xlint:unchecked", "-source", "1.7", "-target", "1.7", "-g:vars")
+
+doc in Compile <<= target.map(_ / "none")
+
+publishArtifact in (Compile, packageSrc) := false
+
+//sublimeTransitive := true
+
+logBuffered in Test := false
+
+Keys.fork in Test := false
+
+parallelExecution in Test := false
+
 seq(Revolver.settings: _*)
+
--- src/test/scala/reactive/socket/PlainSocketTest.scala
+++ src/test/scala/reactive/socket/PlainSocketTest.scala
@@ -1,22 +1,27 @@
 package reactive.socket
 
-import reactive.Configuration
-import reactive.api.{ MainActors, ReactiveApi }
-import reactive.find.FindActor
-import reactive.hide.HideActor
+import java.net.{InetSocketAddress, Socket, URI}
+
 import akka.actor.ActorSystem
-import akka.io.{ IO, Tcp }
-import java.net.{ InetSocketAddress, Socket, URI }
+import akka.io.{IO, Tcp}
 import org.java_websocket.client.WebSocketClient
 import org.java_websocket.handshake.ServerHandshake
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
+import reactive.Configuration
+import reactive.api.{MainActors, ReactiveApi}
+import reactive.find.FindActor
+import reactive.hide.HideActor
 
 @RunWith(classOf[JUnitRunner])
 class PlainSocketTest extends FunSuite with MainActors with ReactiveApi {
   implicit lazy val system = ActorSystem("reactive-system")
-  sys.addShutdownHook({system.shutdown})
+
+  sys.addShutdownHook({
+    system.shutdown
+  })
+
   test("pure socket connection") {
     val rs = new ReactiveServer(Configuration.portWs)
     rs.forResource("/find/ws", Some(find))
@@ -25,36 +30,47 @@ class PlainSocketTest extends FunSuite with MainActors with ReactiveApi {
     find ! FindActor.Clear
     hide ! HideActor.Clear
     Thread.sleep(1000L) // wait for all servers to be cleanly started
+
     var wsmsg = ""
     val wsf = new WebSocketClient(URI.create(s"ws://localhost:${Configuration.portWs}/find/ws")) {
-      override def onMessage(msg : String) {
+      override def onMessage(msg: String) {
         wsmsg = msg
       }
-      override def onOpen(hs : ServerHandshake) {}
-      override def onClose(code : Int, reason : String, intentional : Boolean) {}
-      override def onError(ex : Exception) {}
+
+      override def onOpen(hs: ServerHandshake) {}
+
+      override def onClose(code: Int, reason: String, intentional: Boolean) {}
+
+      override def onError(ex: Exception) {}
     }
     wsf.connect
+
     val conn = new Socket
     conn.connect(new InetSocketAddress("localhost", Configuration.portTcp))
     conn.getOutputStream.write("2.1523721 41.4140567\n".getBytes)
     conn.getOutputStream.flush
     Thread.sleep(1000L)
+
     val first = """\{"move":\{"id":"[-0-9a-f]+","idx":"C","longitude":2\.1523721,"latitude":41\.4140567\}\}""".r
     assert(None != first.findFirstIn(wsmsg))
     Thread.sleep(1000L)
+
     conn.getOutputStream.write("-38.4798 -3.8093\n".getBytes)
     conn.getOutputStream.flush
     Thread.sleep(1000L)
+
     val second = """\{"move":\{"id":"[-0-9a-f]+","idx":"C","longitude":-38\.4798,"latitude":-3\.8093\}\}""".r
     assert(None != second.findFirstIn(wsmsg))
     Thread.sleep(1000L)
+
     conn.close
     Thread.sleep(1000L)
+
     val clear = """\{"clear":\{"id":"[-0-9a-f]+","idx":"C"\}\}""".r
     assert(None != clear.findFirstIn(wsmsg))
     wsf.close
     Thread.sleep(1000L)
+
     rs.stop
   }
 }
